63121ad022692d2e39cb1b62db239c671faeddd9
Giovanni Atzori <plaintesi@gmail.com>
2017-07-24 19:10:14 +0200

    ricerca beacon e nearable

--- app/src/main/java/spano/unica/it/plain/MainActivity.java
+++ app/src/main/java/spano/unica/it/plain/MainActivity.java
@@ -15,6 +15,7 @@ import android.widget.TextView;
 
 import com.estimote.coresdk.common.config.EstimoteSDK;
 import com.estimote.coresdk.common.requirements.SystemRequirementsChecker;
+import com.estimote.coresdk.observation.region.Region;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -35,13 +36,35 @@ import java.util.HashMap;
 import java.util.Map;
 
 
+
+import android.app.Application;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
+import com.estimote.coresdk.observation.region.beacon.BeaconRegion;
+import com.estimote.coresdk.recognition.packets.Beacon;
+import com.estimote.coresdk.service.BeaconManager;
+import com.estimote.coresdk.recognition.packets.Packet;
+
+import java.util.List;
+import java.util.UUID;
+
+
+
+
 public class MainActivity extends AppCompatActivity {
-    private final long SCANTIME = 2000;
+    private final long SCANTIME = 6000;
     private final long WAITTIME = 1000;
     private static final String TAG = "MainActivity";
     private AulaManager aulamanager;
     private WebSocketClient mWebSocketClient;
     private Student student = new Student();
+
+
+    private BeaconManager beaconManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +74,13 @@ public class MainActivity extends AppCompatActivity {
         student.setSurname("Fanni");
         student.setId(24);
         student.getStickerList().add("B");
+
+
+
+
+        beaconManager = new BeaconManager(getApplicationContext());
+
+
         connectWebSocket();
         //mWebSocketClient.onMessage("Ciao Marteena");
 
@@ -59,7 +89,9 @@ public class MainActivity extends AppCompatActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }*/
-        Button doveSOno = (Button) findViewById(R.id.sonoQui);
+
+
+        /*Button doveSOno = (Button) findViewById(R.id.sonoQui);
         doveSOno.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -78,17 +110,111 @@ public class MainActivity extends AppCompatActivity {
             }
 
 
+        });*/
+        EstimoteSDK.initialize(getApplicationContext(), "plain-e3d", "4180e1971192b9fc330cb6210dc79916");
+        Button scanNearable = (Button) findViewById(R.id.scanNearable);
+        scanNearable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("button scan nearable");
+
+                Map<NearableID, Banco> banchi = new HashMap<>();
+                NearableID nearable1 = new NearableID("283913311736b9e9");
+                Banco banco1 = new Banco("Banco n°1");
+
+                banchi.put(nearable1, banco1);
+                aulamanager = new AulaManager(getApplicationContext(), banchi);
+                aulamanager.startUpdates();
+                aulamanager.setListener(new AulaManager.Listener() {
+                    @Override
+                    public void nearableTrovato(Banco banco, NearableID nearable) {
+                        //((TextView) findViewById(R.id.titleLabel)).setText(banco.getNomeBanco());
+                        //((TextView) findViewById(R.id.descriptionLabel)).setText(" beacon con id: " + nearable.getNearableIDString());
+                        findViewById(R.id.descriptionLabel).setVisibility(View.VISIBLE);
+                    }
+
+                    @Override
+                    public void nearableNontrovato(Banco banco) {
+                        //((TextView) findViewById(R.id.titleLabel)).setText("Nessun nearable nelle vicinanze");
+                        //findViewById(R.id.descriptionLabel).setVisibility(View.INVISIBLE);
+                    }
+                });
+            }
+
+
         });
 
 
-        Map<NearableID, Banco> banchi = new HashMap<>();
-        NearableID nearable1 = new NearableID("283913311736b9e9");
-        Banco banco1 = new Banco("Banco n°1");
+        Button scanBeacon = (Button) findViewById(R.id.scanBeacon);
+        scanBeacon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("button scan beacon");
+                beaconManager = new BeaconManager(getApplicationContext());
 
-        banchi.put(nearable1, banco1);
-        EstimoteSDK.initialize(getApplicationContext(), "plain-e3d", "4180e1971192b9fc330cb6210dc79916");
+                beaconManager.setMonitoringListener(new BeaconManager.BeaconMonitoringListener() {
+                    public void showNotification(String title, String message) {
+                        System.out.println("mostrando la notifica");
+                        Intent notifyIntent = new Intent(getApplicationContext(), MainActivity.class);
+                        notifyIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                        PendingIntent pendingIntent = PendingIntent.getActivities(getApplicationContext(), 0,
+                                new Intent[] { notifyIntent }, PendingIntent.FLAG_UPDATE_CURRENT);
+                        Notification notification = new Notification.Builder(getApplicationContext())
+                                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                                .setContentTitle(title)
+                                .setContentText(message)
+                                .setAutoCancel(true)
+                                .setContentIntent(pendingIntent)
+                                .build();
+                        notification.defaults |= Notification.DEFAULT_SOUND;
+                        notification.defaults |= Notification.DEFAULT_VIBRATE;
+                        NotificationManager notificationManager =
+                                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                        notificationManager.notify(1, notification);
+                    }
+                    @Override
+                    public void onEnteredRegion(BeaconRegion region, List<Beacon> list) {
+                        System.out.println("entrato nella regione");
+                        for (Beacon beacon : list) {
 
-        aulamanager = new AulaManager(this, banchi);
+                            System.out.println(beacon.getProximityUUID()+ " " + beacon.getUniqueKey() + " " + beacon.getMajor() + " " + beacon.getMinor() + " " + beacon.getRssi());
+
+                        }
+                        showNotification(
+                                "Buongiorno Djanni!",
+                                "Bentornato in batcaverna");
+
+                    }
+                    @Override
+                    public void onExitedRegion(BeaconRegion region) {
+                        showNotification(
+                                "Arrivederci Djanni!",
+                                "Torna presto a trovarci");
+                    }
+                });
+
+                beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+                    @Override
+                    public void onServiceReady() {
+                        beaconManager.startMonitoring(new BeaconRegion(
+                                "StanzaDueBatcave",
+                                UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"),
+                                null, null));
+                        System.out.println("cercando beacon");
+                    }
+                });
+
+            }
+
+
+        });
+
+
+        beaconManager.setForegroundScanPeriod(SCANTIME, WAITTIME);
+
+
+
+
         /*aulamanager.setListener(new AulaManager.Listener(){
             @Override
             public void nerableTrovati(Banco banco, NearableID nearable) {
@@ -99,22 +225,9 @@ public class MainActivity extends AppCompatActivity {
 
         });*/
 
-        aulamanager.setListener(new AulaManager.Listener() {
-            @Override
-            public void nearableTrovato(Banco banco, NearableID nearable) {
-                //((TextView) findViewById(R.id.titleLabel)).setText(banco.getNomeBanco());
-                //((TextView) findViewById(R.id.descriptionLabel)).setText(" beacon con id: " + nearable.getNearableIDString());
-                findViewById(R.id.descriptionLabel).setVisibility(View.VISIBLE);
-            }
 
-            @Override
-            public void nearableNontrovato(Banco banco) {
-                //((TextView) findViewById(R.id.titleLabel)).setText("Nessun nearable nelle vicinanze");
-                //findViewById(R.id.descriptionLabel).setVisibility(View.INVISIBLE);
-            }
-        });
 
-        //beaconManager.setForegroundScanPeriods(SCANTIME, WAITTIME);
+
 
 
 
@@ -124,6 +237,8 @@ public class MainActivity extends AppCompatActivity {
                     .commit();
         }*/
     }
+
+
 
     @Override
     protected void onResume() {
@@ -135,6 +250,9 @@ public class MainActivity extends AppCompatActivity {
             Log.e(TAG, "If this is fixable, you should see a popup on the app's screen right now, asking to enable what's necessary");
         } else {
             Log.d(TAG, "Start updates");
+            if(aulamanager== null)
+                System.out.println("ciao");
+            else
             aulamanager.startUpdates();
         }
     }
@@ -143,6 +261,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         Log.d(TAG, "Stopping ShowroomManager updates");
+        if(aulamanager== null)
+            System.out.println("ciao");
+        else
         aulamanager.stopUpdates();
     }
 
@@ -174,7 +295,6 @@ public class MainActivity extends AppCompatActivity {
         try {
             //uri = new URI("ws://echo.websocket.org");
             uri = new URI("ws://192.168.1.143:8080/PlainWeb/endpoint");
-            System.out.println("Apertooo22222");
         } catch (URISyntaxException e) {
             e.printStackTrace();
             return;
